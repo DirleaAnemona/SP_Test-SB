5f7e0fa34980275397246c4b588ec423cb8b1811
DirleaAnemona <73755374+DirleaAnemona@users.noreply.github.com>
2022-02-19 15:52:04 +0200

    Add files via upload

---
+++ src/HR.java
@@ -0,0 +1,28 @@
+public class HR {
+    private String name;
+    private int salariu;
+    private int vechimea;
+
+    public HR(String name, int salariu, int vechimea){
+        this.name = name;
+        this.salariu = salariu;
+        this.vechimea = vechimea;
+    }
+    public String setName(String Name) {
+        this.name = name;
+        return name;
+    }
+    public String setsalariu(int salariu) {
+        this.salariu = salariu;
+        return salariu;
+    }
+
+    public String setvechimea(int vechimea) {
+        this.vechimea = vechimea;
+        return vechimea;
+    }
+
+    public String toString() {
+        return ("Nume: " + this.name + " salariu: " + this.salariu + " vechimea: " + this.vechimea);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Manager M = new Manager("Gheorghe" ,"5000", "6");
+
+    }
+}
